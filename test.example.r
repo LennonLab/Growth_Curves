cf01e68d851d8c050c8d580eea7620b6b8016b08
Mario Muscarella <mmuscare@indiana.edu>
2014-03-27 14:19:57 -0400

    changed directories

--- test.example.r
+++ test.example.r
@@ -6,16 +6,14 @@
 ################################################################################
 #                                                                              #
 #	Written by: M. Muscarella                                                    #
-#   Based on growthcurve_code.R Written by: M. Larsen (2013/07/18)             #
-#                                                                              #
-#	Last update: 2/19/14                                                         #
+#	  Last update: 2/19/14                                                       #
 #                                                                              #
 ################################################################################
 
 # Load Dependencies
-source("./scripts/modified_Gomp.r")
+source("../scripts/modified_Gomp.r")
 # Create Directory For Output
-dir.create("./output", showWarnings = FALSE)
+dir.create("../output", showWarnings = FALSE)
 
 # Run Example
-growth.modGomp("GrowthCurve_Example.txt", "test")
+growth.modGomp("../data/GrowthCurve_Example.txt", "test")
