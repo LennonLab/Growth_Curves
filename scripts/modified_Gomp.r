9e2f8d7e5a05f52ad4fd43b19c7e40bcb17aebf0
Mario Muscarella <mmuscare@indiana.edu>
2014-12-04 08:33:31 -0500

    minor changes to reading in files

--- scripts/modified_Gomp.r
+++ scripts/modified_Gomp.r
@@ -8,100 +8,85 @@
 #	Written by: M. Muscarella                                                    #
 #   Based on growthcurve_code.R Written by: M. Larsen (2013/07/18)             #
 #                                                                              #
-#	Last update: 3/3/14                                                          #
+#	Last update: 3/3/14                                                         #
 #                                                                              #
 ################################################################################
 
-growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1, 
-  synergy=T , smooth=F, start = " ", t.cut = "2"){
+growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1, synergy=T, skip = ""){
 # Input = Raw txt output file from Synergy MX
 # Intercept.guess = initial guess of non-grid parameter for y intercept '
-# synergy=T --> the data comes from the synergy mx machine. 
+# synergy=T --> the data comes from the synergy mx machine.
 #   If false data should be deliminated in proper format
 # Load Code Dependencies
-  source("../scripts/read.synergy.R")
-  source("../scripts/curve_fit_fxs.R")
-  source("../scripts/grid.mle2.R")
-  
-# Data Input 
-  data.in <- read.synergy(input, start)
-  temp.test <- lm(round(Temp, 2) ~ Time, data=data.in)  
+  source("../bin/read.synergy.R")
+  source("../bin/curve_fit_fxs.R")
+  source("../bin/grid.mle2.R")
+
+# Data Input
+  data.in <- read.synergy(input, skip = skip)
+  temp.test <- lm(round(Temp, 2) ~ Time, data=data.in)
   p <- round(anova(temp.test)$'Pr(>F)'[1], 3)
   temp.min <- min(data.in$Temp)
   temp.max <- max(data.in$Temp)
   temp.diff <- temp.max - temp.min
-  if (temp.diff < 3) {} else {stop("Stop, check for temperature effects")}
+  if (temp.diff < 2) {} else {stop("Stop, check for temperature effects")}
   samples <- colnames(data.in[3:dim(data.in)[2]])
-     
+
   # Initialize Data Storage
-  results <- matrix(NA,nrow=1,ncol=10)
+  results <- matrix(NA,nrow=(dim(data.in)[2])-2,ncol=10)
   colnames(results) <- c("Curve","b0","A","umax","L","z",
     "umax.lw","umax.up","umax.lw.FI","umax.up.FI")
   results <- as.data.frame(results)
-  
+
   # Creat Output
   outfile <- paste("../output/",output,".txt", sep="")
-  titles <- c("Curve","b0","A","umax","L","z", "umax.lw","umax.up","umax.lw.FI",
-    "umax.up.FI")
-  write.table(as.matrix(t(titles)), file=outfile, append=T, row.names=F, 
-    col.names=F, sep=",", quote=FALSE)  
-  
+  titles <- c("Curve","b0","A","umax","L","z", "umax.lw","umax.up","umax.lw.FI","umax.up.FI")
+  write.table(as.matrix(t(titles)), file=outfile, append=T, row.names=F, col.names=F, sep=",", quote=FALSE)
+
   for(i in 3:dim(data.in)[2]){
     # Print Operation Status
-    print(paste(round(((i-2)/(dim(data.in)[2]-2)*100),2),"% complete", 
-      sep = ""), quote=F)
-    
+    print(paste(round(((i-2)/(dim(data.in)[2]-2)*100),2),"% complete", sep = ""), quote=F)
+
     # Extract Data
     t <- data.in$Time
     s <- data.in[,i]
-    realdata <- data.frame(t,s)
-    
-    if (smooth == TRUE){
+    if (max(s) - min(s) < 0.2) next
+    else realdata <- data.frame(t,s)
+
     # Smoothing Function
     s.2 <- as.numeric(filter(s, rep(1/11,11), circular=F, sides=2))
     s.2[1:5] <- s[1:5]
     s.2[(length(s.2)-5):length(s.2)] <- s[(length(s)-5):length(s)]
     s.max <- max(which(s.2 == max(s.2, na.rm=T)))
-    t.end <- round(t[s.max],0) + 2
-    t <- t[which(t <= t.end)]
-    s <- s.2[which(t <= t.end)]
-    tmpdata <- data.frame(t, s)
-    } else {
-    s.max <- max(which(s == max(s, na.rm=T)))
-    t.end <- round(t[s.max],0) + t.cut
-    t <- t[which(t <= t.end)]
-    s <- s[which(t <= t.end)]    
-    tmpdata <- data.frame(t,s)}
-    
-    # Check for Flat, Blank Curves
-    if(diff(range(tmpdata$s)) > 0.05){    
+    t.end <- round(t[s.max],0) + 1
+    t.trim <- t[which(t <= t.end)]
+    s.trim <- s.2[which(t <= t.end)]
+    tmpdata <- data.frame(t.trim, s.trim)
+    #plot(s.trim ~ t.trim, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, data=tmpdata)
 
-    # Set Grid and Start Lists for Model
-    grids1<-list(umax=c(0.01,0.05,0.1),L=c(-20,-2,0.1,2,20),z=c(-5,-0.5))
-    start1<-list(b0=intercept.guess,A=max(tmpdata[,2]),umax=NA,L=NA,z=NA)
+  # Set Grid and Start Lists for Model
+	 # Modified Gompertz, dat~dnorm(mean=m.gomp(time2,c(b0,A,umax,L)),sd=exp(z))
+	 grids1<-list(umax=c(0.05,0.1,1),L=c(-5,-0.5,0.1,5,10,20),z=c(-2,-0.5))
+	 start1<-list(b0=intercept.guess,A=max(tmpdata[,2]),umax=NA,L=NA,z=NA)
 
   # Perform grid.mle2 Fits
-    fit1<-grid.mle2(minuslogl=s~dnorm(mean=m.gomp(t,c(b0,A,umax,L)),sd=exp(z)),
-      grids=grids1,start=start1,data=tmpdata,method="BFGS")
-    print(paste("finished", colnames(data.in)[i], "fit"))	
+    fit1<-grid.mle2(minuslogl=s.trim~dnorm(mean=m.gomp(t.trim,c(b0,A,umax,L)),sd=exp(z)),grids=grids1,start=start1,data=tmpdata,method="BFGS")
+    print("finished fit")
 
 	# isolate best of each class of model
-    best.f1<-fit1$res.mod[[which(fit1$res.mat[,'AIC'] == 
-      min(fit1$res.mat[,'AIC']))[1]]]
+    best.f1<-fit1$res.mod[[which(fit1$res.mat[,'AIC']==min(fit1$res.mat[,'AIC']))[1]]]
 
 	# generate plot of model fits
-    plot(s ~ t, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, 
-      data=realdata)
+    plot(s ~ t, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, data=realdata)
     curve(m.gomp(x,coef(best.f1)[1:4]),0,max(realdata$t),col='blue',lwd=2,add=T)
-    pdf(file=paste("../output/", output, colnames(data.in[i]),".pdf",sep=""))
-    plot(s ~ t, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, 
-      data=realdata)
-    curve(m.gomp(x,coef(best.f1)[1:4]),0,max(tmpdata$t),col='blue',lwd=2,add=T)
+    pdf(file=paste("../output/testplot",colnames(data.in[i]),".pdf",sep=""))
+    plot(s ~ t, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, data=realdata)
+    curve(m.gomp(x,coef(best.f1)[1:4]),0,max(tmpdata$t.trim),col='blue',lwd=2,add=T)
     dev.off()
 
 	# attempt profiling
     pf1<-profile(best.f1)
-    print(paste("finished", colnames(data.in)[i], "profile"))
+    print("finished pf1")
 
     if(class(pf1)=="profile.mle2"){
     ci1<-confint(pf1)['umax',]
@@ -110,28 +95,18 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
 		  names(ci1)<-c("2.5 %","97.5 %")
       }
     ciFI1<-confint.FI(best.f1)['umax',]
-    	
+
     # Save coefficients of model
     cfs<-coef(best.f1)
-    results$Curve <- colnames(data.in)[i]	
-    results$b0<-cfs['b0']
-    results$A<-cfs['A']
-    results$umax<-cfs['umax']
-    results$L<-cfs['L']
-    results$z<-cfs['z']
-    results[c("umax.lw","umax.up","umax.lw.FI","umax.up.FI")] <-c (ci1,ciFI1)
-    write.table(results, file=outfile, append=T, row.names=F, col.names=F, 
-      sep=",", quote=FALSE)
-    }else{
-      print(paste(colnames(data.in)[i], "is blank or didn't grow"))
-      }
+    results$Curve[i] <- colnames(data.in)[i]
+    results$b0[i]<-cfs['b0']
+    results$A[i]<-cfs['A']
+    results$umax[i]<-cfs['umax']
+    results$L[i]<-cfs['L']
+    results$z[i]<-cfs['z']
+    results[i,c("umax.lw","umax.up","umax.lw.FI","umax.up.FI")] <-c (ci1,ciFI1)
+    write.table(results[i,], file=outfile, append=T, row.names=F, col.names=F, sep=",", quote=FALSE)
+    }
   results1<-results
   write.csv(results,"results.csv")
-  }
-  print("", quote=F)
-  print("Check for samples that didn't converge", quote=F)
-  print("Likely causes for convergence problems: no detectable lag", quote=F)  
-  print("time, culture is still in exponential growth", quote=F)
-  print("Curves with b0 values <0.0 may need to be repeated", quote=F)
 }
-                                                  
--- scripts/read.synergy.r
+++ scripts/read.synergy.r
@@ -10,19 +10,19 @@
 #   Selects only the matrix of plate data                                      #
 #   Converts time to minutes                                                   #
 #   Checks the type of all values & changes to numeric if needed               #
-#   Outputs data matric {Time, Temp, Well...                                   #                                         
+#   Outputs data matric {Time, Temp, Well...                                   #
 ################################################################################
 
-read.synergy <- function(input = " ", start = " "){
-  data.in <- read.delim(input, skip=(start - 1), header=T, as.is=T)
+read.synergy <- function(input = " ", skip = ""){
+  data.in <- read.delim(input, skip=skip, header=T, as.is=T)
   results.start <- which(data.in == "Results")
   data.out <- data.in[1:(results.start - 1),]
   colnames(data.out)[2] <- "Temp"
   t.h <- as.numeric(lapply(strsplit(data.out$Time, "\\:"), "[", 1))
   t.m <- as.numeric(lapply(strsplit(data.out$Time, "\\:"), "[", 2))
-  data.out$Time <- round(t.h + t.m/60, 2)  
+  data.out$Time <- round(t.h + t.m/60, 2)
   for (i in 1:dim(data.out)[2]){
     if (is.numeric(data.out[,i]) == FALSE){data.out[,i] = as.numeric(data.out[,i])}}
   return(data.out)
-  }                                                            
-  
+  }
+
