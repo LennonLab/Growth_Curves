07f8da83fd51675a3e18b2e1ee37340ab8b3f3a5
lennonj <lennonj@indiana.edu>
2015-01-28 17:18:19 -0500

    JTL and MEM troubleshooting with test files

--- test/test.example.r
+++ test/test.example.r
@@ -20,4 +20,6 @@ source("../bin/modified_Gomp.r")
 dir.create("../output", showWarnings = FALSE)
 
 # Run Example
-growth.modGomp("../data/GrowthCurve_Example.txt", "test", skip=32)
+growth.modGomp("../data/GrowthCurve_Example.txt", "test", skip=31)
+growth.modGomp("../data/GrowthCurve_Example2.txt", "test", skip=48)
+growth.modGomp("../data/RPF.txt", "RPF", skip = 39)
