08cbb571458dc8634e4ed65b35d6b817c5caa89e
Mario Muscarella <mmuscare@indiana.edu>
2015-11-24 12:48:29 -0500

    minor changes; works with both types of input; updated README

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-Growth_Curves
+Bacterial Growth Curves
 =============
 
-Lennon Lab Growth Curve Analysis
+## Lennon Lab Growth Curve Analysis Scripts
+
+The code contained in the repo provides an anlaysis pipeline for bacterial growth curves.
+The code fits a Modified Gompertz Equation to optical density readings.
+The code uses grid searching maximum likelihood estimation to fit the model.
+
+## How to use this code
+
+1. Copy the contents of `bin` into your project
+2. Use `test.example.R` as a template for your analysis
+3. Proceed with analysis
+
+## Repo Contents
+
+* **bin**
+		* *modified_Gomp.R*: function for analysis pipeline
+		* *read.synergy.R*: contains code for parsing raw data from synergy MX
+		* *grid.mle2.R*: contains code to grid search maximum likelihood
+		* *curve_fit_fxs.R*: contains the growth model equations		
+
+* **data**
+		* Various raw data examples
+
+* **test**
+		* *test.example.R*: A script with example code
+
+
+## Contributors
+
+[Mario Muscarella](http://mmuscarella.github.io/): Ph.D. candidate in the [Lennon Lab](http://www.indiana.edu/~microbes/people.php).
+
+[Dr. Jay Lennon](http://www.indiana.edu/~microbes/people.php): Principle Investigator, Associate Professor, Department of Biology, Indiana University, Bloomington. Head of the [Lennon Lab](http://www.indiana.edu/~microbes/people.php).
+
+Venus Kuo: Ph.D. student in the [Lennon Lab](http://www.indiana.edu/~microbes/people.php).
+
+## License
+
+Please see LICENSE.md
--- bin/modified_Gomp.r
+++ bin/modified_Gomp.r
@@ -13,9 +13,10 @@
 ################################################################################
 
 growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
-                           synergy=T, temp = T, skip = ""){
+                           delta = 0.05, synergy=T, temp = T, skip = ""){
     # Input = Raw txt output file from Synergy MX
     # Intercept.guess = initial guess of non-grid parameter for y intercept
+    # delta = minimum change in OD required for analysis
     # synergy=T --> the data comes from the synergy mx machine.
     #   If false data should be deliminated in proper format
     # temp = T -> is there temperature data include for QC purposes
@@ -26,14 +27,14 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
   source("../bin/grid.mle2.R")
 
   # Data Input
-  if (syntergy = T){
+  if (synergy == T){
     data.in <- read.synergy(input, skip = skip)
     samples <- colnames(data.in[3:dim(data.in)[2]])
   } else {
     data.in <- read.csv(input, header=T)
-    samples <- colnames(data.in[2:dim(data.in)[2]]])
+    samples <- colnames(data.in[2:dim(data.in)[2]])
   }
-  if (temp = T){
+  if (temp == T){
     temp.test <- lm(round(Temp, 2) ~ Time, data=data.in)
     p <- round(anova(temp.test)$'Pr(>F)'[1], 3)
     temp.min <- min(data.in$Temp)
@@ -43,7 +44,7 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
   } else {}
 
   # Initialize Data Storage
-  results <- matrix(NA,nrow=(dim(data.in)[2])-2,ncol=10)
+  results <- matrix(NA,nrow=length(samples),ncol=10)
   colnames(results) <- c("Curve","b0","A","umax","L","z",
     "umax.lw","umax.up","umax.lw.FI","umax.up.FI")
   results <- as.data.frame(results)
@@ -56,15 +57,21 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
               col.names=F, sep=",", quote=FALSE)
 
   for(i in 1:length(samples)){
+
     # Print Operation Status
-    print(paste(round(((i)/(length(samples))*100),2),"% complete",
-                sep = ""), quote=F)
+    print(paste("Starting sample ", samples[i], sep = ""), quote=F)
 
     # Extract Data
     t <- data.in$Time
-    s <- data.in[,i]
-    if (max(s) - min(s) < 0.05) next
-    else realdata <- data.frame(t,s)
+    s <- data.in[,which(colnames(data.in) == samples[i])]
+    if (max(s) - min(s) < delta) {
+      plot(s ~ t, main=samples[i], ylab="ABS", xlab="Time", pch=19)
+      print(paste("Observed change in OD is not greater than ", delta,
+                  " in sample ", samples[i], sep=""), quote=F)
+      next
+    } else {
+      realdata <- data.frame(t,s)
+    }
 
     # Smoothing Function
     s.2 <- as.numeric(filter(s, rep(1/11,11), circular=F, sides=2))
@@ -75,7 +82,6 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
     t.trim <- t[which(t <= t.end)]
     s.trim <- s.2[which(t <= t.end)]
     tmpdata <- data.frame(t.trim, s.trim)
-    #plot(s.trim ~ t.trim, main=colnames(data.in[i]), ylab="ABS", xlab="Time", pch=19, data=tmpdata)
 
     # Set Grid and Start Lists for Model
     # Modified Gompertz, dat~dnorm(mean=m.gomp(time2,c(b0,A,umax,L)),sd=exp(z))
@@ -86,7 +92,7 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
     fit1<-grid.mle2(minuslogl=s.trim~dnorm(mean=m.gomp(t.trim,c(b0,A,umax,L)),
                     sd=exp(z)),grids=grids1,start=start1,data=tmpdata,
                     method="BFGS")
-    print("finished fit")
+    print("finished fit", quote=F)
 
 	  # isolate best of each class of model
     best.f1<-fit1$res.mod[[which(fit1$res.mat[,'AIC']==min(fit1$res.mat[,'AIC']))[1]]]
@@ -94,14 +100,14 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
 	  # generate plot of model fits
     plot(s ~ t, main=samples[i], ylab="ABS", xlab="Time", pch=19, data=realdata)
     curve(m.gomp(x,coef(best.f1)[1:4]),0,max(realdata$t),col='blue',lwd=2,add=T)
-    pdf(file=paste("../output/testplot",colnames(data.in[i]),".pdf",sep=""))
+    pdf(file=paste("../output/testplot",samples[i],".pdf",sep=""))
     plot(s ~ t, main=samples[i], ylab="ABS", xlab="Time", pch=19, data=realdata)
     curve(m.gomp(x,coef(best.f1)[1:4]),0,max(tmpdata$t.trim),col='blue',lwd=2,add=T)
     dev.off()
 
 	# attempt profiling
     pf1<-profile(best.f1)
-    print("finished pf1")
+    print("finished pf1", quote=F)
 
     if(class(pf1)=="profile.mle2"){
     ci1<-confint(pf1)['umax',]
@@ -121,6 +127,10 @@ growth.modGomp <- function(input=" ", output=" ", intercept.guess=0.1,
     results$z[i]<-cfs['z']
     results[i,c("umax.lw" , "umax.up" , "umax.lw.FI" , "umax.up.FI")] <-c (ci1,ciFI1)
     write.table(results[i,], file=outfile, append=T, row.names=F, col.names=F, sep=",", quote=FALSE)
+
+    # Print Operation Status
+    print(paste(round(((i)/(length(samples))*100),0),"% complete",
+                sep = ""), quote=F)
     }
   results1<-results
   write.csv(results,"results.csv")
