20ba68fb824cd683c74b125333731bf589b1c0f2
lennonj <lennonj@indiana.edu>
2017-05-15 23:39:40 -0400

    Pseudos for LTDE

--- test/test.example.R
+++ test/test.example.R
@@ -28,6 +28,7 @@ growth.modGomp("../test/125_1_6_GrowthCurves.txt", "BehriTest", skip = 38)
 
 # Run Example (CSV file)
 growth.modGomp("../data/ControlRpf.csv", "csv_test", synergy=F, temp=F)
+growth.modGomp("../data/Pseudo.csv", "csv_test", synergy=F, temp=F, smooth=F)
 
 # Run Example (CSV file) w/o Smoothing
 growth.modGomp("../data/ControlRpf.csv", "csv_test", synergy=F, temp=F, smooth=F)
