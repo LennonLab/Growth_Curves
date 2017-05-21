723201a21bc9597b1314cbc01d4c8047376b632d
Venus Kuo <vkuo126@gmail.com>
2017-05-21 16:39:20 -0400

    Venus update

--- test/test.example.R
+++ test/test.example.R
@@ -12,10 +12,18 @@
 
 # Setup Work Environment
 rm(list=ls())
-setwd("~/GitHub/Growth_Curves/test")
+setwd("C:/Users/Venus/Github/Growth_Curves/test")
 
 # Load Dependencies
 source("../bin/modified_Gomp.R")
+
+# Run Example (CSV file)
+# CSV file must have the following format: Header followed by whole or decimal numbers 
+# Time  A1  A2  A3  ...
+# 30    0.2 0.1 0.4 ...
+growth.modGomp("../data/Brentgrowthcurve.csv", "csv_test", synergy=F, temp=F)
+
+growth.modGomp("../data/20170305_GeneralityRpf.csv", "csv_test", synergy=F, temp=F)
 
 # Run Example (Synergy)
 growth.modGomp("../data/GrowthCurve_Example.txt", "test", skip=31)
@@ -23,8 +31,5 @@ growth.modGomp("../data/GrowthCurve_Example2.txt", "test", skip=48)
 growth.modGomp("../data/RPF.txt", "RPF", skip = 39)
 growth.modGomp("../test/125_1_6_GrowthCurves.txt", "BehriTest", skip = 38)
 
-# Run Example (CSV file)
-growth.modGomp("../data/ControlRpf.csv", "csv_test", synergy=F, temp=F)
-
 # Run Example (CSV file) w/o Smoothing
-growth.modGomp("../data/ControlRpf.csv", "csv_test", synergy=F, temp=F, smooth=F)
+growth.modGomp("../data/Brentgrowthcurve2.csv", "csv_test", synergy=F, temp=F, smooth=F)
